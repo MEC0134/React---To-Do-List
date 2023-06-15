e6b5919b337992fe02fb09da65a73d05ba2e15e2
M. Enes Celik <m96celik@gmail.com>
2023-06-15 14:02:06 -0700

    Updates

---
+++ precache-manifest.7eeec0b2033fbbe0726e2c6db6175ffd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fd0c547426cf42f1a68",
+    "url": "/React---To-Do-List/static/js/main.2fd0c547.chunk.js"
+  },
+  {
+    "revision": "59d0cb09d3b1e634cbb6",
+    "url": "/React---To-Do-List/static/js/1.59d0cb09.chunk.js"
+  },
+  {
+    "revision": "292a695894683eff0186",
+    "url": "/React---To-Do-List/static/js/runtime~main.292a6958.js"
+  },
+  {
+    "revision": "8404612640500c078a7c510093dc844b",
+    "url": "/React---To-Do-List/index.html"
+  }
+];
